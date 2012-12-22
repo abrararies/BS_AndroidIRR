8c0c3c4300b7e5275e664f2af4ca6185b6977a01
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-12-22 10:58:40 +0100

    Comitted old changes. Added camera controls to app.

--- src/dtu/biosys/androidirr/BS_AndroidIRRActivity.java
+++ src/dtu/biosys/androidirr/BS_AndroidIRRActivity.java
@@ -1,7 +1,15 @@
 package dtu.biosys.androidirr;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class BS_AndroidIRRActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +18,24 @@ public class BS_AndroidIRRActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, actionCode);
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+    
+    private void handleSmallCameraPhoto(Intent intent) {
+        Bundle extras = intent.getExtras();
+        mImageBitmap = (Bitmap) extras.get("data");
+        mImageView.setImageBitmap(mImageBitmap);
+    }
 }
+
